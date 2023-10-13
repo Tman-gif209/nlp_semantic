8e3b77f03da3d28878be564f11d09142ac2e79c5
Tman-gif209 <146207399+Tman-gif209@users.noreply.github.com>
2023-10-13 17:00:39 +0200

    Add files via upload

---
+++ semantic.py
@@ -0,0 +1,9 @@
+import spacy
+npl = spacy.load('en_core_web_md')
+
+#Write a note about what you found interesting about the similarities between cat, monkey and banana and think of an example of your own.
+    #What I found interesting about the similarities between cat, monkey and banana is that the similarities wont be high between cat and banana since cats dont eat bananas and cats arent a fruit, however banana and monkey will have a high simirlarity since monkeys eat bananas. Monkey and cat will have a high similarity since they are both mammals.
+    #An example of my own is wooden chair, tree and bird. Wooden chair and tree will have a high similarity since the chair is made of wood which comes from a tree. Tree and bird will have a high similarity since birds use the trees for shelter. Bird and wooden chair will have a low similarity since  birds dont use chairs to sit on.
+
+#Run the example file with the simpler language model ‘en_core_web_sm’ and write a note on what you notice is different from the model 'en_core_web_md'.
+    #The difference is the loading time since 'en_core_web_sm' is the default which has less mb. 'en_core_web_md' has a larger mb so it loads a lot more slower.
